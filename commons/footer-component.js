f857b0fe1b645309c28c8d1acd63596e56991007
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2024-06-14 00:11:05 +0200

    inline component' styles

--- commons/footer-component.js
+++ commons/footer-component.js
@@ -5,108 +5,97 @@ createWebComponent(
     `
     <head>
         <link rel="stylesheet" href="commons/styles/global.css"/>
-        <link rel="stylesheet" href="commons/styles/typographic.css"/>
-        <link rel="stylesheet" href="commons/styles/footer.css"/>
     </head>
 
-    <footer class="footer">
-        <div class="footer-copyright">Copyright &copy; 2024</div>
+    <style>
+        footer {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            font-size: var(--text-small-size);
+            padding-top: 1.5rem;
+            gap: 1rem;
 
-        <div class="footer-contacts">
-            <ul class="footer-contacts-list">
-                <li class="footer-contacts-entity">
+            @media (max-width: 640px) {
+                flex-direction: column-reverse;
+            }
+
+            nav {
+                display: flex;
+                gap: 1.5rem;
+                justify-content: center;
+            }
+
+            ul {
+                display: flex;
+                gap: 1.5rem;
+                padding: 0;
+                margin: 0;
+            }
+
+            li {
+                padding: 0;
+                margin: 0;
+            }
+
+            svg {
+                width: 1rem;
+                height: 1rem;
+            }
+        }
+    </style>
+
+    <footer>
+        <div>Copyright &copy; 2024</div>
+
+        <div id="footer-contacts">
+            <ul>
+                <li>
                     <a href="https://www.github.com/anton-bogomazov"
                        target="_blank"
                        rel="noopener noreferrer">
-                        <svg class="footer-contacts-icon"
-                             aria-hidden="true"
-                             focusable="false"
-                             height="24px"
-                             width="24px"
-                             xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor"
-                             fill="none"
-                             stroke-linecap="round"
-                             stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+                        <svg aria-hidden="true" height="24px" width="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M0 0h24v24H0z" stroke="none"/>
+                            <path d="M9 19c-4.3 1.4-4.3-2.5-6-3m12 5v-3.5c0-1 .1-1.4-.5-2 2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2 4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6-.6.6-.6 1.2-.5 2V21"/>
                         </svg>
                     </a>
                 </li>
-                <li class="footer-contacts-entity">
+                <li>
                     <a href="https://www.linkedin.com/in/abogomazov"
                        target="_blank"
                        rel="noopener noreferrer">
-                        <svg class="footer-contacts-icon"
-                             aria-hidden="true"
-                             focusable="false"
-                             height="20px"
-                             width="20px"
-                             xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 24 24"
-                             stroke-width="1.5"
-                             stroke="currentColor"
-                             fill="none"
-                             stroke-linecap="round"
-                             stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
-                            <path d="M8 11l0 5"></path>
-                            <path d="M8 8l0 .01"></path>
-                            <path d="M12 16l0 -5"></path>
-                            <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+                        <svg aria-hidden="true" height="24px" width="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M0 0h24v24H0z" stroke="none"/>
+                            <path d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zM8 11v5M8 8v.01M12 16v-5"/>
+                            <path d="M16 16v-3a2 2 0 0 0-4 0"/>
                         </svg>
                     </a>
                 </li>
-                <li class="footer-contacts-entity">
+                <li>
                     <a href="https://www.t.me/antonbogomazov"
                        target="_blank"
                        rel="noopener noreferrer">
-                        <svg class="footer-contacts-icon"
-                             aria-hidden="true"
-                             focusable="false"
-                             height="24px"
-                             width="24px"
-                             xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 24 24"
-                             stroke-width="1.5"
-                             stroke="currentColor"
-                             fill="none"
-                             stroke-linecap="round"
-                             stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4"></path>
+                        <svg aria-hidden="true" height="24px" width="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M0 0h24v24H0z" stroke="none"/>
+                            <path d="m15 10-4 4 6 6 4-16-18 7 4 2 2 6 3-4"/>
                         </svg>
                     </a>
                 </li>
-                <li class="footer-contacts-entity">
+                <li>
                     <a href="mailto:abogomazov.dev@gmail.com">
-                        <svg class="footer-contacts-icon"
-                             aria-hidden="true"
-                             focusable="false"
-                             height="24px"
-                             width="24px"
-                             xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 24 24"
-                             stroke-width="1.5"
-                             stroke="currentColor"
-                             fill="none"
-                             stroke-linecap="round"
-                             stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <rect x="3" y="5" width="18" height="14" rx="2"></rect>
-                            <polyline points="3 7 12 13 21 7"></polyline>
+                        <svg aria-hidden="true" height="24px" width="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M0 0h24v24H0z" stroke="none"/><rect x="3" y="5" width="18" height="14" rx="2"/>
+                            <path d="m3 7 9 6 9-6"/>
                         </svg>
                     </a>
                 </li>
             </ul>
         </div>
 
-        <nav class="footer-menu" aria-label="More on this site">
-            <a href="index.html" class="footer-menu-entity">Home</a>
-            <a href="experience.html" class="footer-menu-entity">Experience</a>
-            <a href="posts.html" class="footer-menu-entity">Blog</a>
+        <nav aria-label="More on this site">
+            <a href="index.html">Home</a>
+            <a href="experience.html">Experience</a>
+            <a href="posts.html">Blog</a>
         </nav>
 
     </footer>
--- commons/header-component.js
+++ commons/header-component.js
@@ -5,16 +5,44 @@ createWebComponent(
     `
     <head>
         <link rel="stylesheet" href="commons/styles/global.css"/>
-        <link rel="stylesheet" href="commons/styles/typographic.css"/>
-        <link rel="stylesheet" href="commons/styles/header.css"/>
     </head>
 
-    <header class="header">
-        <div class="header-title">
+    <style>
+        header {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 0.5rem;
+            padding-bottom: 1.5rem;
+
+            p {
+                margin: 0;
+                font-weight: 700;
+                line-height: 2rem;
+            }
+
+            nav {
+                display: flex;
+                align-items: center;
+                gap: 1.5rem;
+                top: 3.5rem;
+            }
+        }
+
+        #header-title {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+        }
+    </style>
+
+    <header>
+        <div id="header-title">
             <p class="text-larger">Anton Bogomazov</p>
             <p class="text-large">Software Engineer</p>
         </div>
-        <nav id="navigation-menu" class="header-menu" aria-label="Main menu">
+        <nav id="navigation-menu" aria-label="Main menu">
             <a class="header-menu-entity" href="index.html" rel="prefetch">Home</a>
             <a class="header-menu-entity" href="experience.html" rel="prefetch">Experience</a>
             <a class="header-menu-entity" href="posts.html" rel="prefetch">Blog</a>
